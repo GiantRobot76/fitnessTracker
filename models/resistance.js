5a502d674e5debf650485fe51d0135667e54e8b3
Jon Bowman <jon.m.bowman@gmail.com>
2021-06-11 20:57:45 -0600

    Directory Skeleton and Tentative Resistance Model

---
+++ models/resistance.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ResistanceSchema = new Schema({
+  name: {
+    type: String,
+    required: "Name of Exercise is Required",
+  },
+  weight: {
+    type: Number,
+    required: "Weight is Required",
+  },
+  sets: {
+    type: Number,
+    required: "Number of Sets is Required",
+  },
+  reps: {
+    type: Number,
+    required: "Number of Reps is Required",
+  },
+  Duration: {
+    type: Number,
+    required: "Duration of Exercise is Required",
+  },
+});
+
+const Resistance = mongoose.model("Resistance", ResistanceSchema);
+
+module.exports = Book;
